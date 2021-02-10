b79c4b928a01ae8fd068d31b7f5c202b5e316fb7
Mitchell Gonzalez <mitchell.gonzalez@gmail.com>
2021-02-10 19:51:04 +0000

    added client.js

--- play.js
+++ play.js
@@ -1,18 +1,7 @@
-const net = require('net');
+const connect = require('./client');
+console.log('Connecting ...');
+connect();
 
 /**
  * Establishes connection with the game server
  */
-const connect = function() {
-  const conn = net.createConnection({ 
-    host: 'localhost',
-    port: 50541
-  });
-  // interpret incoming data as text
-  conn.setEncoding('utf8'); 
-
-  return conn;
-}
-
-console.log('Connecting ...');
-connect();
